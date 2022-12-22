f4000d6eb4431d56bedab4ead44bd7f14152983e
AmarrPrime <zloybirik@gmail.com>
2022-12-22 18:47:59 +0300

    done

--- lesson2/zadacha3.py
+++ lesson2/zadacha3.py
@@ -4,3 +4,16 @@
 
 # - Для n=4 {1: 2, 2: 2.25, 3: 2.37, 4: 2.44}
 #         Сумма 9.06
+
+
+# n = int(input())
+# lst = [round((1+1/i)**i, 3) for i in range(1, n+1)]
+# print(f'Последовательность: {lst}\nСумма: {round(sum(lst))}')
+
+
+n = int(input())
+dict_num = {}
+for i in range(1, n+1):
+    dict_num[i] = round((1+1/i)**i, 2)
+print(dict_num)
+print(sum(dict_num.values()))
