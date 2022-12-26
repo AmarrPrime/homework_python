c1f36b6895120f22bda09dfa920166da2b5175f0
AmarrPrime <zloybirik@gmail.com>
2022-12-26 21:36:23 +0300

    done

--- lesson3/task2.py
+++ lesson3/task2.py
@@ -4,3 +4,16 @@
 
 # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+
+from random import randint
+
+list_num = [randint(0, 10) for i in range(randint(5, 10))]
+print(list_num)
+
+final = []
+mid = len(list_num)//2 + len(list_num)%2
+
+
+for i in range(mid):
+    final.append(list_num[i]*list_num[-i-1])
+print(final)
