a7b4c18fbcadfdd088297dc2537387a47783bb8d
AmarrPrime <zloybirik@gmail.com>
2023-02-06 19:23:40 +0300

    task2 done

--- lesson6/task2.py
+++ lesson6/task2.py
@@ -1,1 +1,11 @@
 # Найти сумму чисел списка стоящих на нечетной позиции
+
+
+import random
+
+a = [random.randint(1, 10) for i in range(6)]
+print(a)
+result = [value for indx, value in enumerate(a) if indx % 2 == 1]
+print(result)
+result = sum([value for indx, value in enumerate(a) if indx % 2 == 1])
+print(result)
