96b18ff8b68c93d7d3bc646427df129409ad371d
AmarrPrime <zloybirik@gmail.com>
2023-02-06 19:18:02 +0300

    task1 done

--- lesson6/task1.py
+++ lesson6/task1.py
@@ -9,11 +9,11 @@
 
 from functools import reduce
 
-dot_1 = list(map(int, input('Введите две координаты первой точки A, через пробел: ').split())) 
-dot_2 = list(map(int, input('Введите две координаты второй точки B, через пробел: ').split()))
+dot1 = list(map(int, input('Введите две координаты первой точки A, через пробел: ').split())) 
+dot2 = list(map(int, input('Введите две координаты второй точки B, через пробел: ').split()))
 
-def dot_range(dot_1, dot_2):
-     rng = reduce(lambda x, y: (x + y)**(1/2), (map(lambda dot: (dot[1] - dot[0])**2, zip(dot_1, dot_2))))
+def dot_range(dot1, dot2):
+     rng = reduce(lambda x, y: (x + y)**(1/2), (map(lambda dot: (dot[1] - dot[0])**2, zip(dot1, dot2))))
      return round(rng, 2)
      
-print(dot_range(dot_1, dot_2))
+print(dot_range(dot1, dot2))
